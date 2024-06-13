965cb1a23e8f6e782cd82c30afa5457250d63f05
Samuel <samuelfreitassoares@outlook.com>
2024-06-12 22:41:22 -0300

    fixed errors

---
+++ backend/src/main/java/com/sempressa/backend/repository/ParallelRepository.java
@@ -0,0 +1,9 @@
+package com.sempressa.backend.repository;
+
+import com.sempressa.backend.model.Parallel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParallelRepository extends JpaRepository<Parallel, Long> {
+
+
+}
